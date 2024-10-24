e78614398e8235c64a8d96e0fca6d94d47bc2946
neulhi <neu1hi.work@gmail.com>
2024-10-24 15:56:49 +0900

    테마 환경 구성, 폰트 등록

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./**/*.html'],
-  theme: {
-    extend: {},
-  },
+	theme: {
+		extend: {
+			fontFamily: {
+        suit: ['"SUIT Variable"'],
+        pretendard: ['"Pretendard Variable"'],
+      },
+		},
+	},
   plugins: [],
 }
